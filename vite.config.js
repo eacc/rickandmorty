2e12525a923ab44d991c0c26da7c3fde574a6145
Eddy Cespedes <eacc61@gmail.com>
2022-11-19 12:32:39 -0500

    primer commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://eacc.github.io/rick-and-morty/",
+  base: "https://eacc.github.io/rickandmorty/",
   plugins: [react()]
 })
